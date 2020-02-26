2578fa2bf80f7607ff098b7a5d023f8325c7f29c
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-26 17:28:31 +0100

    added double database action local and heroku

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+db;
 
 app.listen(port, () => console.log(`Up and running on port ${port}`));

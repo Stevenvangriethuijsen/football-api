d0e64f02539808b25a2c72db34b968d53dca6a4f
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-27 12:27:50 +0100

    relations defining

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const teamRouter = require("./team/router");
 const bodyParser = require("body-parser");
+const playerRouter = require("./player/router");
 
 const app = express();
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
 app.use(teamRouter);
+app.use(playerRouter);
 
 const port = process.env.PORT || 4000;
 
---
+++ player/router.js
@@ -0,0 +1,42 @@
+const { Router } = require("express");
+const Player = require("./model");
+
+const router = new Router();
+
+router.get("/player", async (req, res, next) => {
+  try {
+    const players = await Player.findAll();
+    if (players) {
+      res.json(players);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/player", async (req, res, next) => {
+  try {
+    const createPlayer = await Player.create(req.body);
+    res.status(201).json(createPlayer);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/player/:id", async (req, res, next) => {
+  try {
+    const playerId = req.params.id;
+    const singlePlayer = await Player.findByPk(playerId);
+    if (singlePlayer) {
+      res.json(singlePlayer);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
